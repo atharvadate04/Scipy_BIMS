7baea997490f7d1cfcb4ea49733ff7b1d8ac1c5a
Jagdish kachhawah <jagdishkachhwah2110@gmail.com>
2024-07-03 14:45:47 +0530

    addProduct Functionality

--- Main.py
+++ Main.py
@@ -3,9 +3,9 @@ from PIL import Image, ImageTk
 from tkinter import ttk 
 import tkinter as tk
 from tkinter.messagebox import askyesno 
-import sqlite3 as sqlite
-connection = sqlite.connect("ProductList.db")
-connection.close()
+import sqlite3 as sql
+
+
 class IMS:
     def __init__(self,main_window):
         self.main_window = main_window
@@ -15,7 +15,21 @@ class IMS:
         self.main_window.configure(bg="#BEE9E8")
         self.titleIcon = PhotoImage(file="SciPy.png")
 
+        def addProduct(event = NONE):
+            conn = sql.connect("ProductList.db") 
+            cursor = conn.cursor() 
+            
 
+            cursor.execute("CREATE TABLE IF NOT EXISTS productlist (pro_name BLOB,id INTEGER, price REAL(5,2), quantity INTEGER)") 
+
+            PrePro = "SELECT * FROM productlist"
+            if PrePro == idBox.get():
+                cursor.execute("UPDATE productlist SET quantity WHERE first_name = 'John';")
+
+            cursor.execute("INSERT INTO productlist VALUES (?,?,?,?)",(nameBox.get(),idBox.get(),priceBox.get(),quantityBox.get())) 
+            conn.commit()
+
+            conn.close()
 
         def show_frame1():
             adminFrame.tkraise() 
@@ -67,8 +81,8 @@ class IMS:
         productPanel = Frame(adminFrame,bd=3,relief=RIDGE)
         productPanel.place(x=5,y=5,width=390,height=607)
 
-        #prdTitle = Product panel Title
-        prdTitle = Label(productPanel,text="-------- Product Panel --------",font=("Arial",15,"bold"),pady=7).pack()
+        #prdTitle = PRODUCT PANEL Title
+        prdTitle = Label(productPanel, text="-------- Product Panel --------", font=("Arial", 15, "bold"), pady=7).pack()
 
         prdName = Label(productPanel, text="Name :", font=("Arial", 11, "bold"))
         prdName.place(x=20, y=80)
@@ -76,34 +90,37 @@ class IMS:
         nameBox.place(x=120, y=80, height=25) 
         nameBox.focus()
 
-        prdId = Label(productPanel,text="ID :",font=("Arial",11,"bold")).place(x=20,y=140)
-        idBox = tk.Entry(productPanel,width=25,bd=1,relief=SOLID,font=("Arial",12)).place(x=120,y=140,height=25)
+        prdId = Label(productPanel, text="ID :", font=("Arial", 11, "bold"))
+        prdId.place(x=20, y=140)
+        idBox = tk.Entry(productPanel, width=25, bd=1, validate=None, relief=SOLID, font=("Arial", 12))
+        idBox.place(x=120, y=140, height=25)
 
+        prdPrice = Label(productPanel, text="Price :", font=("Arial", 11, "bold"))
+        prdPrice.place(x=20, y=200)
+        priceBox = tk.Entry(productPanel, width=25, bd=1, validate=None, relief=SOLID, font=("Arial", 12))
+        priceBox.place(x=120, y=200, height=25)
 
-        prdPrice = Label(productPanel,text="Price :",font=("Arial",11,"bold")).place(x=20,y=200)
-        priceBox = tk.Entry(productPanel,width=25,bd=1,validate=None,relief=SOLID,font=("Arial",12)).place(x=120,y=200,height=25)
+        prdQuantity = Label(productPanel, text="Quantity :", font=("Arial", 11, "bold"))
+        prdQuantity.place(x=20, y=260)
+        quantityBox = tk.Entry(productPanel, width=25, bd=1, validate=None, relief=SOLID, font=("Arial", 12))
+        quantityBox.place(x=120, y=260, height=25)
 
+        addBtn = Button(productPanel, text="ADD", width=8, height=2, relief=RAISED, bg="#1B4965", fg="white", font=("Arial", 11, "bold"), command=addProduct)
+        addBtn.place(x=30, y=420)
+        updateBtn = Button(productPanel, text="UPDATE", width=8, height=2, relief=RAISED, bg="#1B4965", fg="white", font=("Arial", 11, "bold"))
+        updateBtn.place(x=150, y=420)
+        deleteBtn = Button(productPanel, text="DELETE", width=8, height=2, relief=RAISED, bg="#1B4965", fg="white", font=("Arial", 11, "bold"))
+        deleteBtn.place(x=270, y=420)
 
-        prdQuantity = Label(productPanel,text="Quantity :",font=("Arial",11,"bold")).place(x=20,y=260)
-        quantityBox = tk.Entry(productPanel,width=25,bd=1,validate=None,relief=SOLID,font=("Arial",12)).place(x=120,y=260,height=25)
+        productTable = Frame(adminFrame, bd=3, relief=RIDGE)
+        productTable.place(x=409, y=5, width=737, height=607)
 
-        addBtn = Button(productPanel,text="ADD",width=8,height=2,relief=RAISED,bg="#1B4965",fg="white",font=("Arial",11,"bold")).place(x=30,y=420)
-        updateBtn = Button(productPanel,text="UPDATE",width=8,height=2,relief=RAISED,bg="#1B4965",fg="white",font=("Arial",11,"bold")).place(x=150,y=420)
-        deleteBtn = Button(productPanel,text="DELETE",width=8,height=2,relief=RAISED,bg="#1B4965",fg="white",font=("Arial",11,"bold")).place(x=270,y=420)
-
-
-
-        productTable = Frame(adminFrame,bd=3,relief=RIDGE)
-        productTable.place(x=409,y=5,width=737,height=607)
-
-
-
-        prdTableTitle = Label(productTable,text="-------- Products Table --------",font=("Arial",15,"bold"),pady=7).pack()
+        prdTableTitle = Label(productTable, text="-------- Products Table --------", font=("Arial", 15, "bold"), pady=7).pack()
         
         #==================Sql PANEL ===========================================
+    
 
-
-
+        
 
 
 
@@ -155,7 +172,9 @@ class IMS:
 
 
 
-    
+
+
+
 main_window = Tk()
 mainObj=IMS(main_window)
 
